b45d8e72754b524afd079bbf3e4b05e3f50cf892
RomainBoucard <rom.boucard@gmail.com>
2022-11-25 10:15:49 +0100

    Utilisation et compréhension de la fonction strtok

--- TP1_CPP_LEXIQUE.cpp
+++ TP1_CPP_LEXIQUE.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stdio.h>
+#include <string.h>
 #include "utilitaire.hpp"
 #include "lexiqueClass.h"
 
@@ -24,7 +26,7 @@ int main() {
     std::cout << "Hello World!";
     std::cout << "Hello World!" << endl;
 
-    lexiqueClass classe;
+    /*lexiqueClass classe;
     classe.setName("Romain");
     classe.setNumber(15);
     cout << classe.getName() << endl;
@@ -33,6 +35,19 @@ int main() {
     TestClasse.push_back(classe);
     cout << "Vector" << endl;
     cout << TestClasse[0].getName() << endl;
-    cout << TestClasse[0].getNumber() << endl;
+    cout << TestClasse[0].getNumber() << endl;*/
+
+    char str[] ="- This, a sample string.";
+    char str1[] =", Date. truc";
+    char * pch;
+    printf ("Splitting string \"%s\" into tokens:\n",str);
+    pch = strtok (str," ,-.");
+    cout << pch << endl;
+    pch = strtok (str1," ,-.");
+    cout << pch << endl;
+    pch = strtok (NULL, " ,-.");
+    cout << pch << endl;
+    pch = strtok (NULL, " ,-."); //Element suivant de la chaine de caractères, premier parametre : chaine stockée en mémoire à séparer
+    cout << pch;
     return 0;
 }
